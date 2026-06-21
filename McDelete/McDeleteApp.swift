1bfdd76efdf95e0e081264f67c512a8ea2512530
Llaman <rrezamir@gmail.com>
2026-06-21 17:51:12 +0800

    Initial Commit

---
+++ McDelete/McDeleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  McDeleteApp.swift
+//  McDelete
+//
+//  Created by Rezamir on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct McDeleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
