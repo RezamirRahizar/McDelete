985da7a0db3692f8500b09a6e7a9c4f7d779356e
Llaman <rrezamir@gmail.com>
2026-06-23 18:21:32 +0800

    Cleanup folder organization

---
+++ McDelete/Views/TimelineView.swift
@@ -0,0 +1,19 @@
+//
+//  TimelineView.swift
+//  McDelete
+//
+//  Created by Rezamir on 23/06/2026.
+//
+
+import SwiftUI
+
+struct TimelineView:  View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+#Preview {
+    TimelineView()
+}
